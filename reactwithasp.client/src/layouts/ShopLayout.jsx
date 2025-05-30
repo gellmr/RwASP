cd063042fdc96b597c2c17dd60d336f87ffcc4ea
gellmr <gellmr@users.noreply.github.com>
2025-05-30 20:14:49 +0800

    Added cart button to nav bar

--- reactwithasp.client/src/layouts/ShopLayout.jsx
+++ reactwithasp.client/src/layouts/ShopLayout.jsx
@@ -3,6 +3,7 @@ import MgNavBar from "@/main/MgNavBar";
 import "bootstrap/dist/css/bootstrap.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 const ShopLayout = ({ children }) => {
   return (
@@ -10,6 +11,9 @@ const ShopLayout = ({ children }) => {
       <MgNavBar>
         <Nav.Link href="index">Shop</Nav.Link>
         <Nav.Link href="indexAdmin">Admin</Nav.Link>
+        <Nav.Link href="cart">
+          <Button variant="success">Cart</Button>
+        </Nav.Link>
       </MgNavBar>
       <Container>
         <div id="shopLayout" style={{ border: '3px solid lime'}} >
