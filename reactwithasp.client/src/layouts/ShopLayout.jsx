2966718a483167e810f2f999e349329962210763
gellmr <gellmr@users.noreply.github.com>
2025-05-30 17:50:49 +0800

    Whitespace

--- reactwithasp.client/src/layouts/ShopLayout.jsx
+++ reactwithasp.client/src/layouts/ShopLayout.jsx
@@ -5,9 +5,11 @@ const ShopLayout = ({ children }) => {
   return (
     <>
       <NavBar><a href="indexAdmin">Admin Page</a></NavBar>
+      
       <div id="shopLayout" style={{ border: '3px solid lime'}} >
         {children}
       </div>
+      
     </>
   );
 }
