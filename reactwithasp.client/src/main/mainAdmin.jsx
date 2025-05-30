58579fe33acb303a8c45f815cd711998b8c55f33
gellmr <gellmr@users.noreply.github.com>
2025-05-30 17:00:23 +0800

    Added bootstrap and jquery

--- reactwithasp.client/src/main/mainAdmin.jsx
+++ reactwithasp.client/src/main/mainAdmin.jsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// css imports
+import 'bootstrap/dist/css/bootstrap.css';
 import './main.css'
+// js imports
+//import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// other imports
 import AdminApp from '../Admin/AdminApp.jsx'
 
 createRoot(document.getElementById('root')).render(
