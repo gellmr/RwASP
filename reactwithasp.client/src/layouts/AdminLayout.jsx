890cad3266018287cb2750eef4f42aeb8b049bd5
gellmr <gellmr@users.noreply.github.com>
2025-05-30 18:55:40 +0800

    Added react-bootstrap. Added bootstrap Container. Also needed bootstrap to work.
    NOTE, import "bootstrap/dist/css/bootstrap.css" is needed.

--- reactwithasp.client/src/layouts/AdminLayout.jsx
+++ reactwithasp.client/src/layouts/AdminLayout.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import NavBar from "@/main/NavBar";
+import "bootstrap/dist/css/bootstrap.css";
+import Container from 'react-bootstrap/Container';
 
 const AdminLayout = ({ children }) => {
   return (
     <>
       <NavBar><a href="index">Back to Shop</a></NavBar>
-      <div id="adminLayout" style={{ border: '3px solid orange' }} >
-        {children}
-      </div>
+      <Container>
+        <div id="adminLayout" style={{ border: '3px solid orange' }} >
+          {children}
+          </div>
+      </Container>
     </>
   );
 }
--- reactwithasp.client/src/layouts/ShopLayout.jsx
+++ reactwithasp.client/src/layouts/ShopLayout.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import NavBar from "@/main/NavBar";
+import "bootstrap/dist/css/bootstrap.css";
+import Container from 'react-bootstrap/Container';
 
 const ShopLayout = ({ children }) => {
   return (
     <>
       <NavBar><a href="indexAdmin">Admin Page</a></NavBar>
-      
-      <div id="shopLayout" style={{ border: '3px solid lime'}} >
-        {children}
-      </div>
-      
+      <Container>
+        <div id="shopLayout" style={{ border: '3px solid lime'}} >
+          {children}
+        </div>
+      </Container>
     </>
   );
 }
