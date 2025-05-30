a8c27c0be7087c10952add1c4793e6e7f53e7aec
gellmr <gellmr@users.noreply.github.com>
2025-05-30 20:21:53 +0800

    Added Row and Cols to Shop

--- reactwithasp.client/src/layouts/ShopLayout.jsx
+++ reactwithasp.client/src/layouts/ShopLayout.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import MgNavBar from "@/main/MgNavBar";
 import "bootstrap/dist/css/bootstrap.css";
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 
@@ -15,10 +17,13 @@ const ShopLayout = ({ children }) => {
           <Button variant="success">Cart</Button>
         </Nav.Link>
       </MgNavBar>
-      <Container>
-        <div id="shopLayout" style={{ border: '3px solid lime'}} >
-          {children}
-        </div>
+      <Container id="shopLayout" style={{ border: '3px solid lime' }}>
+        <Row>
+          <Col xs={6}>All Products</Col>
+          <Col xs={6}>Category: All</Col>
+        </Row>
+        <hr />
+        {children}
       </Container>
     </>
   );
