a5b1aac52f7f4517e335befe243cb359bda033a9
gellmr <gellmr@users.noreply.github.com>
2025-05-31 15:08:17 +0800

    Update styles for product line

--- reactwithasp.client/src/Shop/InStockProductCanAdd.jsx
+++ reactwithasp.client/src/Shop/InStockProductCanAdd.jsx
@@ -7,11 +7,17 @@ function InStockProductCanAdd({title, slug }) {
   return (
     <Row className="inStockProductCanAdd">
       <Col xs={12} className="productDetailsFlex">
-        <div className="productDetails">
-          <h5>{title}</h5>
-          <span>{slug}</span>
-        </div>
-        <Button variant="success">Add to Cart</Button>
+
+        <Row>
+          <Col xs={12} sm={9}>
+            <div className="productDetails">
+              <h5>{title}</h5>
+              <span>{slug}</span>
+            </div>
+          </Col>
+          <Col xs={12} sm={3}><Button variant="success">Add to Cart</Button></Col>
+        </Row>
+
       </Col>
     </Row>
   );
