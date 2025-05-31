1cadb6c281ada84a5a0de4305b29b8ce4a17b957
gellmr <gellmr@users.noreply.github.com>
2025-05-31 17:31:14 +0800

    Fixed typo

--- reactwithasp.client/src/Shop/ProductsContext.jsx
+++ reactwithasp.client/src/Shop/ProductsContext.jsx
@@ -41,7 +41,7 @@ function productsReducer(products, action) {
 
 const initialProducts = [
   { id: 0, title: 'Soccer Ball $35.00', slug: "FIFA approved size and weight."},
-  { id: 1, title: 'Corner Flags $25.00', slug: "BGive some flourish to your playing field with these coloured corner flags." },
+  { id: 1, title: 'Corner Flags $25.00', slug: "Give some flourish to your playing field with these coloured corner flags." },
   { id: 2, title: 'Referee Whisle $12.00', slug: "For serious games, call it with this chrome Referee Whistle." },
   { id: 3, title: 'Red and Yellow Cards $10.00', slug: "Official size and colour, waterproof high visibility retroflective coating." },
 ];
