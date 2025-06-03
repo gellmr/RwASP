67f1f279802efe62748e8b11f15ae97987f676e9
gellmr <gellmr@users.noreply.github.com>
2025-06-03 14:48:55 +0800

    Added cart bars

--- reactwithasp.client/src/Shop/Cart.jsx
+++ reactwithasp.client/src/Shop/Cart.jsx
@@ -2,16 +2,25 @@ import { useSelector } from 'react-redux'
 import ShopLayout from "@/layouts/ShopLayout";
 import PaginationLinks from "@/Shop/PaginationLinks";
 import CartProduct from "@/Shop/CartProduct";
+import CartBar from "@/Shop/CartBar";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 function Cart() {
   const cartProducts = useSelector(state => state.cart.value); // array of products
   return (
     <ShopLayout>
       <h2>Your Cart:</h2>
       <PaginationLinks />
-      <div className="cartContents">
-        {cartProducts && cartProducts.map(prod =>
-          <CartProduct key={prod.id} title={prod.product.title} slug={prod.product.description} productId={prod.product.id} />
-        )}
+      <div className="col-12">
+        <Row>
+          <CartBar />
+          <Col className="cartContents col-12 col-lg-8">
+            {cartProducts && cartProducts.map(prod =>
+              <CartProduct key={prod.id} title={prod.product.title} slug={prod.product.description} productId={prod.product.id} />
+            )}
+          </Col>
+          <CartBar />
+        </Row>
       </div>
       <PaginationLinks />
     </ShopLayout>
---
+++ reactwithasp.client/src/Shop/CartBar.jsx
@@ -0,0 +1,8 @@
+import Col from 'react-bootstrap/Col'
+
+function CartBar(){
+  return (
+    <Col className="col-2 d-none d-lg-block" style={{ backgroundColor: "grey" }}>BAR</Col>
+  );
+}
+export default CartBar;
