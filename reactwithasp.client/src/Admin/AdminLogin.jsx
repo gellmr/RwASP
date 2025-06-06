8ae236178fa12d049769d75feafb12d6ea749104
gellmr <gellmr@users.noreply.github.com>
2025-06-06 11:45:36 +0800

    Update AdminLogin.jsx

--- reactwithasp.client/src/Admin/AdminLogin.jsx
+++ reactwithasp.client/src/Admin/AdminLogin.jsx
@@ -1,7 +1,29 @@
-function AdminLogin(){  
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+function AdminLogin() {  
   return (
     <>
-      Admin Login Page
+      <h4>Admin Login</h4>
+      <div style={{ display:"flex", justifyContent:"center"}}>
+      <Form style={{ marginTop: 30, marginBottom:50, maxWidth:600}}>
+        <Row>
+          <Col xs={12} md={6}>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={6}>
+            <Form.Group className="mb-3" controlId="formGroupPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+      </div>
     </>
   );
 }
