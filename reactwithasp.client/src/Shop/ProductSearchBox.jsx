58d45ae0a20122c449820981d1907a4c8b28484c
gellmr <gellmr@users.noreply.github.com>
2025-06-06 10:39:44 +0800

    Show current category title

--- reactwithasp.client/src/Shop/ProductSearchBox.jsx
+++ reactwithasp.client/src/Shop/ProductSearchBox.jsx
@@ -1,18 +1,24 @@
+import { useParams } from 'react-router';
+import { useSelector} from 'react-redux'
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function ProductSearchBox() {
+  const { category } = useParams();
+  const categories = useSelector(state => state.categories.value); 
+  const storeCat = categories.find((cat) => cat.segment === category);
+  const displayCat = category === undefined ? "All" : storeCat.title;
   return (
     <div>
-      <h6 style={{color:'grey', textAlign:"left"}}>Category: All</h6>
+      <h6 style={{color:'grey', textAlign:"left"}}>Category: {displayCat}</h6>
 
       <InputGroup className="mb-3">
         <InputGroup.Text id="basic-addon1">
           <i className="bi bi-search"></i>
         </InputGroup.Text>
         <Form.Control
-          placeholder="SearchString"
+          placeholder="Search (Feature under construction)"
           aria-label="SearchString"
           aria-describedby="basic-addon1"
         />
