e9531cd681893505099e4700fbdf534619a18217
gellmr <gellmr@users.noreply.github.com>
2025-06-06 08:27:13 +0800

    Added api/products/category/{category} Route in ASP

--- ReactWithASP.Server/InStockProduct.cs
+++ ReactWithASP.Server/InStockProduct.cs
@@ -6,5 +6,6 @@
     public string Title { get; set; }
     public string Description { get; set; }
     public Decimal Price { get; set; }
+    public Cat Category { get; set; }
   }
 }
--- ReactWithASP.Server/ProductCategory.cs
+++ ReactWithASP.Server/ProductCategory.cs
@@ -1,9 +1,55 @@
 ﻿namespace ReactWithASP.Server
 {
+  public enum Cat
+  {
+    None,
+    Soccer,
+    Chess,
+    WaterSport
+  }
+
   public class ProductCategory
   {
     public Int32 ID { get; set; }
     public string Title { get; set; }
     public string Segment { get; set; }
+
+    public static Cat ParseCat(string str)
+    {
+      Cat myCat;
+      Cat cat = Cat.None;
+      try
+      {
+        if (Enum.TryParse(str, out myCat))
+        {
+          switch (myCat)
+          {
+            case Cat.None: cat = Cat.None; break;
+            case Cat.Soccer: cat = Cat.Soccer; break;
+            case Cat.Chess: cat = Cat.Chess; break;
+            case Cat.WaterSport: cat = Cat.WaterSport; break;
+          }
+          return cat;
+        }
+        throw new Exception("could not parse string as Cat enum");
+      }
+      catch (Exception e)
+      {
+        throw e;
+      }
+    }
+
+    public static string ParseCat(Cat cat)
+    {
+      string myCat = "";
+      switch (cat)
+      {
+        case Cat.None: myCat = "None"; break;
+        case Cat.Soccer: myCat = "Soccer"; break;
+        case Cat.Chess: myCat = "Chess"; break;
+        case Cat.WaterSport: myCat = "WaterSport"; break;
+      }
+      return myCat;
+    }
   }
 }
