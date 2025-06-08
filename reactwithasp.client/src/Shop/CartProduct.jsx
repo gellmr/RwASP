becb88d097fa57498bbf2a80e664760796ba085e
gellmr <gellmr@users.noreply.github.com>
2025-06-09 00:29:52 +0800

    Special style rules for Cart Quantity span. Used custom breakpoint.

--- reactwithasp.client/src/Shop/CartProduct.jsx
+++ reactwithasp.client/src/Shop/CartProduct.jsx
@@ -17,18 +17,19 @@ function CartProduct({ title, slug, productId, qty })
         <Row className="innerRow">
           <Col xs={9}>
             <div data-product-id={productId} className="inCartItemText">
-              <h5>{title}</h5>
+              <h6>{title}</h6>
             </div>
           </Col>
 
           <Col xs={3} style={{ textAlign: "right", display: "flex", justifyContent: "flex-end" }}>
-            <span className="d-block d-sm-none" style={{ fontSize: "13px", paddingTop:"2px" }}>Quantity:</span>
-            <span className="d-none d-sm-block">Quantity:</span>
+            <span className="d-block d-sm-none cartQtyXXs" style={{ fontSize: "13px", paddingTop: "2px" }}>Qty:</span>
+            <span className="cartQtyXs d-block d-sm-none " style={{ fontSize: "13px", paddingTop: "2px" }}>Quantity:</span>
+            <span className="d-none d-sm-block" style={{ fontSize: "14px", paddingTop:"1px" }}>Quantity:</span>
             &nbsp;
             <span style={{ fontWeight:"500" }}>{qty}</span>
           </Col>
 
-          <Col xs={9} style={{ textAlign:"left", fontSize:"13px", paddingRight:"40px", display: "flex", justifyContent: "flex-start" }}>
+          <Col xs={9} style={{ textAlign:"left", fontSize:"13px", paddingRight:"55px", display: "flex", justifyContent: "flex-start" }}>
             <p>{slug}</p>
           </Col>
 
