e17705bdc003c87a19372502b5aa6a350967ccd8
gellmr <gellmr@users.noreply.github.com>
2025-06-09 18:17:16 +0800

    Markup for checkout page

--- reactwithasp.client/src/Shop/Checkout.jsx
+++ reactwithasp.client/src/Shop/Checkout.jsx
@@ -1,15 +1,72 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import CartBar from "@/Shop/CartBar";
+
 function Checkout() {
+
+  const handleCheckoutSubmit = (e) => {    
+  };
+
   return (
     <>
-      <h2>Checkout</h2>
+      <h2 style={{ marginTop: "5px" }}>Checkout</h2>
       <div className="col-12">
         <Row>
           <CartBar />
-          <Col>
-            (Products)
+          <Col className="checkoutLines">
+            <div className="shipHeading">Please provide your details below, and we'll ship your goods right away.</div>
+
+            <h5 className="shipHeading">Please provide your name...</h5>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>First Name</InputGroup.Text>
+              <Form.Control id="shipFirstName" />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Last Name</InputGroup.Text>
+              <Form.Control id="shipLastName" />
+            </InputGroup>
+
+            <h5 className="shipHeading">Shipping Address</h5>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Line 1</InputGroup.Text>
+              <Form.Control id="shipLine1"/>
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Line 2</InputGroup.Text>
+              <Form.Control id="shipLine2" />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Line 3</InputGroup.Text>
+              <Form.Control id="shipLine3" />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Text>City</InputGroup.Text>
+              <Form.Control id="shipCity" />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>State</InputGroup.Text>
+              <Form.Control id="shipState" />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Country</InputGroup.Text>
+              <Form.Control id="shipCountry" />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Zip</InputGroup.Text>
+              <Form.Control id="shipZip" />
+            </InputGroup>
+
+            <h5 className="shipHeading">Your Email Address</h5>
+            <InputGroup className="mb-5">
+              <InputGroup.Text>Email</InputGroup.Text>
+              <Form.Control id="shipEmail" />
+            </InputGroup>
+
+            <Button variant="success" className={"btn btn-primary"} onClick={handleCheckoutSubmit}>Complete Order</Button>
           </Col>
           <CartBar />
         </Row>
