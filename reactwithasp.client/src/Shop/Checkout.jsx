9ff58c91fd04b9b400d56bc3607311d8bbfaef6e
gellmr <gellmr@users.noreply.github.com>
2025-06-09 19:22:27 +0800

    Correct indentation in Form on checkout page

--- reactwithasp.client/src/Shop/Checkout.jsx
+++ reactwithasp.client/src/Shop/Checkout.jsx
@@ -22,54 +22,54 @@ function Checkout() {
             <div className="shipHeading">Please provide your details below, and we'll ship your goods right away.</div>
 
             <Form onSubmit={handleSubmit}>
-            <h5 className="shipHeading">Please provide your name...</h5>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>First Name</InputGroup.Text>
-              <Form.Control id="shipFirstName" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Last Name</InputGroup.Text>
-              <Form.Control id="shipLastName" />
-            </InputGroup>
+              <h5 className="shipHeading">Please provide your name...</h5>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>First Name</InputGroup.Text>
+                <Form.Control id="shipFirstName" />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Last Name</InputGroup.Text>
+                <Form.Control id="shipLastName" />
+              </InputGroup>
 
-            <h5 className="shipHeading">Shipping Address</h5>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Line 1</InputGroup.Text>
-              <Form.Control id="shipLine1"/>
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Line 2</InputGroup.Text>
-              <Form.Control id="shipLine2" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Line 3</InputGroup.Text>
-              <Form.Control id="shipLine3" />
-            </InputGroup>
+              <h5 className="shipHeading">Shipping Address</h5>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Line 1</InputGroup.Text>
+                <Form.Control id="shipLine1"/>
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Line 2</InputGroup.Text>
+                <Form.Control id="shipLine2" />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Line 3</InputGroup.Text>
+                <Form.Control id="shipLine3" />
+              </InputGroup>
 
-            <InputGroup className="mb-3">
-              <InputGroup.Text>City</InputGroup.Text>
-              <Form.Control id="shipCity" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>State</InputGroup.Text>
-              <Form.Control id="shipState" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Country</InputGroup.Text>
-              <Form.Control id="shipCountry" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Zip</InputGroup.Text>
-              <Form.Control id="shipZip" />
-            </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>City</InputGroup.Text>
+                <Form.Control id="shipCity" />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>State</InputGroup.Text>
+                <Form.Control id="shipState" />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Country</InputGroup.Text>
+                <Form.Control id="shipCountry" />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Zip</InputGroup.Text>
+                <Form.Control id="shipZip" />
+              </InputGroup>
 
-            <h5 className="shipHeading">Your Email Address</h5>
-            <InputGroup className="mb-5">
-              <InputGroup.Text>Email</InputGroup.Text>
-              <Form.Control id="shipEmail" />
-            </InputGroup>
+              <h5 className="shipHeading">Your Email Address</h5>
+              <InputGroup className="mb-5">
+                <InputGroup.Text>Email</InputGroup.Text>
+                <Form.Control id="shipEmail" />
+              </InputGroup>
 
-            <Button variant="success" className={"btn btn-primary"} type="submit" >Complete Order</Button>
+              <Button variant="success" className={"btn btn-primary"} type="submit" >Complete Order</Button>
             </Form>
 
           </Col>
