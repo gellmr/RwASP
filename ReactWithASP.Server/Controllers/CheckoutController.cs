46cd830e5d86619c04acc200859a19c218c33ad2
gellmr <gellmr@users.noreply.github.com>
2025-06-25 15:07:19 +0800

    Validation Regex for CheckoutSubmitDTO properties

--- ReactWithASP.Server/Controllers/CheckoutController.cs
+++ ReactWithASP.Server/Controllers/CheckoutController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ReactWithASP.Server.Domain;
 using ReactWithASP.Server.Domain.Abstract;
+using System.ComponentModel.DataAnnotations;
 
 namespace ReactWithASP.Server.Controllers
 {
@@ -17,26 +18,43 @@ namespace ReactWithASP.Server.Controllers
     }
 
     [HttpPost("submit")] // POST api/checkout/submit.  Accepts application/json POST submissions containing stringified JSON data in request body.
-    public IActionResult Submit([FromBody] CheckoutSubmit checkoutSubmit)
+    public IActionResult Submit([FromBody] CheckoutSubmitDTO checkoutSubmit)
     {
+      if (!ModelState.IsValid){
+        return BadRequest(ModelState);
+      }
       Order order = new Order();
       order.OrderedProducts = new List<OrderedProduct>();
       ordersRepo.SaveOrder(order);
       return Ok(checkoutSubmit); // Respond with 200 OK, and automatically cast object to JSON for the response.
     }
 
-    public class CheckoutSubmit
+    public class CheckoutSubmitDTO
     {
-      public string FirstName { get; set; }
-      public string LastName { get; set; }
-      public string ShipLine1 { get; set; }
-      public string ShipLine2 { get; set; }
-      public string ShipLine3 { get; set; }
-      public string ShipCity { get; set; }
-      public string ShipState { get; set; }
-      public string ShipCountry { get; set; }
-      public string ShipZip { get; set; }
-      public string ShipEmail { get; set; }
+      [RegularExpression(@"^[A-Za-z0-9\s\-\.\,\(\)]{1,50}$", ErrorMessage = "Please use alphanumeric, spaces, dashes, period, comma, parentheses, 1-50 characters.")]
+      public string? FirstName { get; set; }
+      [RegularExpression(@"^[A-Za-z0-9\s\-\.\,\(\)]{1,50}$", ErrorMessage = "Please use alphanumeric, spaces, dashes, period, comma, parentheses, 1-50 characters.")]
+      public string? LastName { get; set; }
+
+      [RegularExpression(@"^[A-Za-z0-9\s\-\.\,\(\)\:\/]{1,100}$", ErrorMessage = "Please use alphanumeric, spaces, dashes, period, comma, parentheses, colon, forward slash, 1-100 characters.")]
+      public string? ShipLine1 { get; set; }
+      [RegularExpression(@"^[A-Za-z0-9\s\-\.\,\(\)\:\/]{1,100}$", ErrorMessage = "Please use alphanumeric, spaces, dashes, period, comma, parentheses, colon, forward slash, 1-100 characters.")]
+      public string? ShipLine2 { get; set; }
+      [RegularExpression(@"^[A-Za-z0-9\s\-\.\,\(\)\:\/]{1,100}$", ErrorMessage = "Please use alphanumeric, spaces, dashes, period, comma, parentheses, colon, forward slash, 1-100 characters.")]
+      public string? ShipLine3 { get; set; }
+
+      [RegularExpression(@"^[A-Za-z0-9\s\-\.\,\(\)]{1,50}$", ErrorMessage = "Please use alphanumeric, spaces, dashes, period, comma, parentheses, 1-50 characters.")]
+      public string? ShipCity { get; set; }
+      [RegularExpression(@"^[A-Za-z0-9\s\-\.\,\(\)]{1,50}$", ErrorMessage = "Please use alphanumeric, spaces, dashes, period, comma, parentheses, 1-50 characters.")]
+      public string? ShipState { get; set; }
+      [RegularExpression(@"^[A-Za-z0-9\s\-\.\,\(\)]{1,50}$", ErrorMessage = "Please use alphanumeric, spaces, dashes, period, comma, parentheses, 1-50 characters.")]
+      public string? ShipCountry { get; set; }
+
+      [RegularExpression(@"^[0-9]{4}$", ErrorMessage = "Please provide a 4 digit number.")]
+      public string? ShipZip { get; set; }
+
+      [RegularExpression(@"^[a-zA-Z0-9\.\-]+@[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?){0,4}$", ErrorMessage = "Please provide a valid email address.")]
+      public string? ShipEmail { get; set; }
     }
   }
 }
