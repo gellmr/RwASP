dde7861b815aa9a265e0c6e84c1041ccfc434403
gellmr <gellmr@users.noreply.github.com>
2025-07-15 15:58:07 +0800

    Fixed display problem for pagination links

--- reactwithasp.client/src/Shop/PaginationLinks.jsx
+++ reactwithasp.client/src/Shop/PaginationLinks.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router";
 function PaginationLinks({ numPages, currPage=1, myRoute="/" })
 {
   const listItems = Array.from({ length: numPages }).map((_, index) => (
-    <NavLink to={myRoute + (index + 1)} key={index + 1} className={(currPage === index + 1) ? "btn btn-primary" : "btn btn-default"}>
+    <NavLink to={myRoute + (index + 1)} key={index + 1} className={(parseInt(currPage) === index + 1) ? "btn btn-primary" : "btn btn-default"}>
       {index + 1}
     </NavLink>
   ));
