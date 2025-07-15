cd86d854f9098041f41c673d0e9c01c0982e6d9a
gellmr <gellmr@users.noreply.github.com>
2025-07-16 01:00:37 +0800

    Placeholder markup for Myorders

--- reactwithasp.client/src/Shop/Myorders.jsx
+++ reactwithasp.client/src/Shop/Myorders.jsx
@@ -1,9 +1,30 @@
 import AdminTitleBar from "@/Admin/AdminTitleBar";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Myorders = () => {
   return (
     <>
-      <AdminTitleBar titleText="My Orders" />
+      <Row style={{height:650}}>
+        <Col xs={12}>
+          <AdminTitleBar titleText="My Orders" />
+        </Col>
+
+        <Col xs={12}>
+          <div>Order 1</div>
+          <div>Order 2</div>
+          <div>Order 3</div>
+          <div>Order 4</div>
+          <div>Order 5</div>
+          <br />
+          <div>Order 1</div>
+          <div>Order 2</div>
+          <div>Order 3</div>
+          <div>Order 4</div>
+          <div>Order 5</div>
+          <br />
+        </Col>
+      </Row>
     </>
   );
 }
