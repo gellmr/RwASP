8a5288e3a621a6eca92f3799c64ebb0c96097742
gellmr <gellmr@users.noreply.github.com>
2025-07-18 18:08:22 +0800

    Use active style for All Products when nothing selected

--- reactwithasp.client/src/Shop/MgCategoryMenu.jsx
+++ reactwithasp.client/src/Shop/MgCategoryMenu.jsx
@@ -1,10 +1,16 @@
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { NavLink } from "react-router";
-function MgCategoryMenu({ isVertical=false, children }) {
+import { useParams } from 'react-router';
+
+function MgCategoryMenu({ isVertical = false, children })
+{
+  const { category } = useParams();
+  const allProdStyles = (category === undefined) ? "btn btn-light active" : "btn btn-light";
+
   return (
     <>
       <ButtonGroup vertical={isVertical} className="mg-category-menu-r">
-        <NavLink to={"/"} key={crypto.randomUUID()} data-catid={0} className={"btn btn-light"}>All Products</NavLink>
+        <NavLink to={"/"} key={crypto.randomUUID()} data-catid={0} className={allProdStyles}>All Products</NavLink>
         {children}
       </ButtonGroup>
     </>
