5512f47f8eeaeb70811a880789d335d7878abd11
gellmr <gellmr@users.noreply.github.com>
2025-07-18 18:54:56 +0800

    Added mgAdminNavHideMD style to shop button

--- reactwithasp.client/src/Shop/ShopButton.jsx
+++ reactwithasp.client/src/Shop/ShopButton.jsx
@@ -2,14 +2,26 @@ import { NavLink } from "react-router";
 
 function ShopButton({ withBackArrow })
 {
+  const backToShopMarkup = function (text) {
+    return (
+      <>
+        <span className="mgAdminNavHideMD">Back to</span>
+        &nbsp;
+        Shop
+      </>
+    );
+  }
+
   const iconElement = withBackArrow ? <i className="bi bi-arrow-left-short"></i> : <></>;
-  const linkText = withBackArrow ? "Back to Shop" : "Shop";
+  const linkText = withBackArrow ? backToShopMarkup() : "Shop";
 
   return (
     <>
       <NavLink to="/" className="mgNavLinkBtn">
-        {iconElement}
-        {linkText}
+        <span style={{ minWidth:45}}>
+          {iconElement}
+          {linkText}
+        </span>
       </NavLink>
     </>
   );
