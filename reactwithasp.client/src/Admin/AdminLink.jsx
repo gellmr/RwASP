2206ec030299cc195292454ffe665b2836462723
gellmr <gellmr@users.noreply.github.com>
2025-07-18 12:12:39 +0800

    Custom nav bar items for logged in / not logged in / admin pages

--- reactwithasp.client/src/Admin/AdminLink.jsx
+++ reactwithasp.client/src/Admin/AdminLink.jsx
@@ -2,23 +2,40 @@ import React from 'react';
 import { useSelector } from 'react-redux'
 import { NavLink } from "react-router";
 import VL from "@/Shop/VL";
+import { useLocation } from 'react-router';
 
 const AdminLink = () => {
+  const location = useLocation();
   const login = useSelector(state => state.login.value);
+  const withinAdmin = location.pathname.indexOf("/admin") !== -1;
+  const isLoggedIn = login !== "";
+
+  const adminLinks = (
+    <>
+      <NavLink to="/admin/products" className="mgNavLinkBtn" >Products</NavLink><VL />
+      <NavLink to="/admin/orders" className="mgNavLinkBtn" >Orders</NavLink><VL />
+      <NavLink to="/admin/useraccounts" className="mgNavLinkBtn" >User Accounts</NavLink>
+    </>
+  );
 
   const adminLink = function () {
-    if (login === "") {
+    if (isLoggedIn && withinAdmin) {
+      return ( // Logged in and currently on the admin pages
+        <>
+          {adminLinks}
+        </>
+      );
+    } else if (isLoggedIn) { // Logged in but not on the admin pages
       return (
         <>
-          <NavLink to="/myorders" className="mgNavLinkBtn" >My Orders</NavLink>
+          <NavLink to="/myorders" className="mgNavLinkBtn" >My Orders</NavLink><VL />
+          {adminLinks}
         </>
       );
     }
-    return (
+    return ( // Not logged in
       <>
-        <NavLink to="/admin/products" className="mgNavLinkBtn" >Products</NavLink><VL />
-        <NavLink to="/admin/orders" className="mgNavLinkBtn" >Orders</NavLink><VL />
-        <NavLink to="/admin/useraccounts" className="mgNavLinkBtn" >User Accounts</NavLink>
+        <NavLink to="/myorders" className="mgNavLinkBtn" >My Orders</NavLink>
       </>
     );
   }
