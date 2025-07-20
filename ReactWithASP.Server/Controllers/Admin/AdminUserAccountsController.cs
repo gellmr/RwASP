60443ff4af862a1a1e384ff0ada25c20760ceb00
gellmr <gellmr@users.noreply.github.com>
2025-07-20 20:51:58 +0800

    Added AdminUserAccountsController, update AdminUserAccounts

---
+++ ReactWithASP.Server/Controllers/Admin/AdminUserAccountsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReactWithASP.Server.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+
+namespace ReactWithASP.Server.Controllers.Admin
+{
+  [Authorize]
+  [ApiController]
+  [Route("api")]
+  public class AdminUserAccountsController : AdminBaseController
+  {
+    protected UserManager<AppUser> _userManager;
+
+    public AdminUserAccountsController(UserManager<AppUser> userManager){
+      _userManager = userManager;
+    }
+
+    [HttpGet("admin-useraccounts")]
+    public ActionResult GetUserAccounts()
+    {
+      try
+      {
+        IEnumerable<AppUser> users = _userManager.Users.ToList();
+        return Ok(users);
+      }
+      catch (Exception ex)
+      {
+        return this.StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+      }
+    }
+  }
+}
--- reactwithasp.client/src/Admin/AdminUserAccounts.jsx
+++ reactwithasp.client/src/Admin/AdminUserAccounts.jsx
@@ -1,9 +1,84 @@
+import { useState, useEffect } from 'react';
 import AdminTitleBar from "@/Admin/AdminTitleBar";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
 
-const AdminUserAccounts = () => {
+const AdminUserAccounts = () =>
+{
+  const retryThisPage = 5;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const axiosInstance = axios.create({});
+  axiosRetry(axiosInstance, { retries: retryThisPage, retryDelay: axiosRetry.exponentialDelay, onRetry: (retryCount, error, requestConfig) => {
+    console.log(`axiosRetry attempt ${retryCount} for ${requestConfig.url}`); 
+  }});
+
+  useEffect(() => {
+    fetchAdminUserAccs();
+  }, []);
+
+  async function fetchAdminUserAccs() {
+    setError("");
+    setIsLoading(true);
+    const url = window.location.origin + "/api/admin-useraccounts";
+    axiosInstance.get(url).then((response) => {
+      console.log('Data fetched:', response.data);
+      //dispatch(setAdminUserAccounts(response.data));
+    })
+    .catch((error) => {
+      console.error('Request failed after retries:', error);
+      setError(error);
+      //dispatch(setAdminUserAccounts([]));
+    })
+    .finally(() => {
+      console.log('Request (and retries) completed. This runs regardless of success or failure.');
+      setIsLoading(false);
+    });
+  }
+
+  const userRowMarkup = (user) => (
+    <>
+      <div>{user.id}</div>
+    </>
+  );
+
+  const userTableMarkup = () => (
+    <>
+      <Row>
+        <Col xs={0} lg={2}>
+          {/*LSPACE*/}
+        </Col>
+        <Col xs={12} lg={8}>
+          { /*
+            userAccounts.map(user => userRowMarkup(user))
+            */
+          }
+        </Col>
+        <Col xs={0} lg={2}>
+          {/*RSPACE*/}
+        </Col>
+      </Row>
+    </>
+  );
+
+  const markup = (isLoading) ? <div className="fetchErr">Loading...</div>             : (
+    (error)                  ? <div className="fetchErr">Error: {error.message}</div> : (
+    userTableMarkup()
+  ));
+
   return (
     <>
-      <AdminTitleBar titleText="User Accounts" />
+      <Row>
+        <Col xs={12}>
+          <AdminTitleBar titleText="User Accounts" />
+        </Col>
+        <Col xs={12}>
+          {markup}
+        </Col>
+      </Row>
     </>
   );
 }
