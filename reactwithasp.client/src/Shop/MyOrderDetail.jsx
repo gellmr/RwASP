933171de26c45dc842d3f0f9673756485a2525f6
gellmr <gellmr@users.noreply.github.com>
2025-07-25 14:50:30 +0800

    Update MyOrderDetail.jsx

--- reactwithasp.client/src/Shop/MyOrderDetail.jsx
+++ reactwithasp.client/src/Shop/MyOrderDetail.jsx
@@ -1,8 +1,10 @@
+import { useParams } from 'react-router';
 
 function MyOrderDetail() {
+  const { orderid } = useParams();
   return (
     <>
-      My Order Detail
+      My Order Detail {orderid}
     </>
   );
 }
