74d50e5c6104c986d759f2669da3ece7c89cd79b
gellmr <gellmr@users.noreply.github.com>
2025-08-01 18:30:33 +0800

    Use import for axiosInstance

--- reactwithasp.client/src/Admin/AdminProducts.jsx
+++ reactwithasp.client/src/Admin/AdminProducts.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { setAdminProducts } from '@/features/admin/products/adminProductsSlice.jsx'
-import axios from 'axios';
-import axiosRetry from 'axios-retry';
+import { axiosInstance } from '@/axiosDefault.jsx';
 import AdminTitleBar from "@/Admin/AdminTitleBar";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -10,17 +9,11 @@ import Image from 'react-bootstrap/Image';
 
 const AdminProducts = () =>
 {
-  const retryThisPage = 5;
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const adminProducts = useSelector(state => state.adminProducts.value);
-
-  const axiosInstance = axios.create({});
-  axiosRetry(axiosInstance, { retries: retryThisPage, retryDelay: axiosRetry.exponentialDelay, onRetry: (retryCount, error, requestConfig) => {
-    console.log(`axiosRetry attempt ${retryCount} for ${requestConfig.url}`); 
-  }});
 
   useEffect(() => {
     fetchAdminProducts();
