32fedb1080bbe9e7251e0f80a9155f070d02bf15
gellmr <gellmr@users.noreply.github.com>
2025-08-01 18:38:20 +0800

    Use import for axiosInstance

--- reactwithasp.client/src/Shop/LogOutButton.jsx
+++ reactwithasp.client/src/Shop/LogOutButton.jsx
@@ -2,23 +2,16 @@ import { NavLink } from "react-router";
 import { useNavigate } from "react-router";
 import { useDispatch } from 'react-redux'
 import { setLogin } from '@/features/login/loginSlice.jsx'
-import axios from 'axios';
-import axiosRetry from 'axios-retry';
+import { axiosInstance } from '@/axiosDefault.jsx';
 
 function LogOutButton()
 {
-  const retryThisPage = 5;
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  // Configure axios instance.
-  axiosRetry(axios, { retries: retryThisPage, retryDelay: axiosRetry.exponentialDelay, onRetry: (retryCount, error, requestConfig) => {
-    console.log(`axiosRetry attempt ${retryCount} for ${requestConfig.url}`);
-  }});
-
   const logoutClick = function () {
     const url = window.location.origin + "/api/admin-logout";
-    axios.post(url).then((response) => {
+    axiosInstance.post(url).then((response) => {
       console.log("------------------------------");
       console.log('Logout success. ', response.data);
       dispatch(setLogin(null));
