e10ce7202d722367955f8134ad4704dac2b7d681
gellmr <gellmr@users.noreply.github.com>
2025-08-02 00:09:19 +0800

    Placeholder members for DTO object

--- ReactWithASP.Server/Controllers/DTO.cs
+++ ReactWithASP.Server/Controllers/DTO.cs
@@ -143,8 +143,11 @@ namespace ReactWithASP.Server.DTO
     {
       public string? Email { get; set; }
       public bool? EmailConfirmed { get; set; }
+      // public Guest? Guest
       public Guid? GuestID { get; set; }
       public string? Id { get; set; }
+      // public string? LockoutEnd
+      // Orders
       public string? PhoneNumber { get; set; }
       public bool? PhoneNumberConfirmed { get; set; }
       public string? Picture { get; set; }
