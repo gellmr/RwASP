0642b891e510467292e0dc57d38be61a4e434d93
gellmr <gellmr@users.noreply.github.com>
2025-08-04 19:32:59 +0800

    Refactored User Pic modal into DragDropUserPicModal component

--- reactwithasp.client/src/Admin/AdminUserAccounts.jsx
+++ reactwithasp.client/src/Admin/AdminUserAccounts.jsx
@@ -1,14 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { setAdminUserAccounts, updateUserPic } from '@/features/admin/useraccounts/adminUserAccountsSlice.jsx'
+import { setAdminUserAccounts } from '@/features/admin/useraccounts/adminUserAccountsSlice.jsx'
 import { axiosInstance } from '@/axiosDefault.jsx';
 import AdminTitleBar from "@/Admin/AdminTitleBar";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Dropzone from 'react-dropzone'
+import DragDropUserPicModal from "@/DragDropUserPicModal";
 import { useNavigate, NavLink } from "react-router";
 
 import '@/AdminUserShared.css'
@@ -16,6 +14,8 @@ import '@/AdminUserAccounts.css'
 
 const AdminUserAccounts = () =>
 {
+  const modalRef = useRef();
+
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -23,53 +23,11 @@ const AdminUserAccounts = () =>
   const userAccounts = useSelector(state => state.adminUserAccounts.users);
   const loginValue = useSelector(state => state.login.value);
 
+  const myUserId = (loginValue === null) ? undefined : loginValue.appUserId;
+
   const navigate = useNavigate();
   if (loginValue === null) {
     navigate('/admin');
-  }
-
-  const myUserId = (loginValue === null) ? undefined : loginValue.appUserId;
-  const isGoogleSignIn = (loginValue === null) ? false : (loginValue.loginType === 'Google Sign In');
-
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  function handleDrop(acceptedFiles)
-  {
-    const file = acceptedFiles[0];
-    if (file)
-    {
-      const url = window.location.origin + "/api/admin-userpic";
-      const formData = new FormData();
-      formData.append('file', file);
-      const postConfig = {
-        headers: { 'Content-Type': 'multipart/form-data' },
-        //onUploadProgress: (progressEvent) => {
-        //  const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-        //  console.log(`   Uploading: ${percentCompleted}%`);
-        //}
-      };
-      axiosInstance.post(url, formData, postConfig).then((response) => {
-
-        // To help debug the production configuration for file upload path
-        let dev_UploadPath  = response.data.debug === 'C:\\path to\\                           RwASP\\reactwithasp.client\\public\\userpic';
-        let prod_UploadPath = response.data.debug === 'C:\\path to\\RwASP-wwwroot\\wwwroot\\userpic';
-
-        console.log('File uploaded successfully!', response.data);
-        dispatch(updateUserPic({
-          userId: response.data.userId,
-          picture: response.data.picture
-        }));
-      })
-      .catch((error) => {
-        console.error('Request failed after retries.', error);
-      })
-      .finally(() => {
-        console.error('Completed - handleDrop');
-        handleClose();
-      });
-    }
   }
 
   useEffect(() => {
@@ -94,54 +52,9 @@ const AdminUserAccounts = () =>
       setIsLoading(false);
     });
   }
-
-  const googleProfileWarn = () => (
-    <div>
-      <b>
-        This will not update your Google Profile image.
-      </b>
-    </div>
-  );
-
-  const modalMarkup = () => (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>
-          Profile Image
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div>Please choose an image for your user profile.<br/>
-          {isGoogleSignIn && googleProfileWarn()}
-        </div>
-        <br/>
-        <Dropzone onDrop={handleDrop}>
-          {({ getRootProps, getInputProps }) => (
-            <section>
-              <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                <div style={{marginBottom:10}}>Click here to browse for a file, or drag and drop to upload.</div>
-                <div className="dragDropCont">
-                  <Image className="cloudGraphic" src={'/graphics/cloud-upload.png'} rounded />
-                </div>
-              </div>
-            </section>
-          )}
-        </Dropzone>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Cancel
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          &nbsp;Save&nbsp;
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
-
+  
   const handleClickPhoto = function () {
-    handleShow();
+    modalRef.current.showModal();
   }
 
   const userRowMarkup = (user, isCurrentUser) => (
@@ -184,7 +97,7 @@ const AdminUserAccounts = () =>
           {/*LSPACE*/}
         </Col>
         <Col xs={12} lg={8}>
-          {modalMarkup()}
+          <DragDropUserPicModal ref={modalRef} />
           {userAccounts.map(user => userRowMarkup(user, (user.id == myUserId)))}
         </Col>
         <Col xs={0} lg={2}>
---
+++ reactwithasp.client/src/DragDropUserPicModal.jsx
@@ -0,0 +1,117 @@
+import { useState, forwardRef, useImperativeHandle } from 'react';
+import { axiosInstance } from '@/axiosDefault.jsx';
+import { useSelector, useDispatch } from 'react-redux'
+import { updateUserPic } from '@/features/admin/useraccounts/adminUserAccountsSlice.jsx'
+import Modal from 'react-bootstrap/Modal';
+import Dropzone from 'react-dropzone'
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
+const DragDropUserPicModal = forwardRef((props, ref) => {
+  // Expose these functions to parent component.
+  useImperativeHandle(ref, () => ({
+    showModal() {
+      setShow(true);
+    },
+    hideModal() {
+      setShow(false);
+    }
+  }));
+
+  const handleClose = () => setShow(false);
+
+  const [show, setShow] = useState(false);
+  const dispatch = useDispatch();
+  const loginValue = useSelector(state => state.login.value);
+  const isGoogleSignIn = (loginValue === null) ? false : (loginValue.loginType === 'Google Sign In');
+
+  const googleProfileWarn = () => (
+    <div>
+      <b>
+        This will not update your Google Profile image.
+      </b>
+    </div>
+  );
+
+  function handleDrop(acceptedFiles) {
+    const file = acceptedFiles[0];
+    if (file) {
+      const url = window.location.origin + "/api/admin-userpic";
+      const formData = new FormData();
+      formData.append('file', file);
+      const postConfig = {
+        headers: { 'Content-Type': 'multipart/form-data' },
+        //onUploadProgress: (progressEvent) => {
+        //  const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+        //  console.log(`   Uploading: ${percentCompleted}%`);
+        //}
+      };
+      axiosInstance.post(url, formData, postConfig).then((response) => {
+
+        // To help debug the production configuration for file upload path
+        let dev_UploadPath = response.data.debug === 'C:\\path to\\                           RwASP\\reactwithasp.client\\public\\userpic';
+        let prod_UploadPath = response.data.debug === 'C:\\path to\\RwASP-wwwroot\\wwwroot\\userpic';
+
+        console.log('File uploaded successfully!', response.data);
+        dispatch(updateUserPic({
+          userId: response.data.userId,
+          picture: response.data.picture
+        }));
+      })
+      .catch((error) => {
+        console.error('Request failed after retries.', error);
+      })
+      .finally(() => {
+        console.log('Completed - handleDrop');
+        handleClose();
+      });
+    }
+  }
+
+  const ddModalMarkup = function () {
+    return (
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Profile Image
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>Please choose an image for your user profile.<br />
+            {isGoogleSignIn && googleProfileWarn()}
+          </div>
+          <br />
+          <Dropzone onDrop={handleDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <div style={{ marginBottom: 10 }}>Click here to browse for a file, or drag and drop to upload.</div>
+                  <div className="dragDropCont">
+                    <Image className="cloudGraphic" src={'/graphics/cloud-upload.png'} rounded />
+                  </div>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            &nbsp;Save&nbsp;
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  return (
+    <>
+      {ddModalMarkup()}
+    </>
+  );
+});
+
+export default DragDropUserPicModal;
