4d3201bb197d37bc49b677d9e271ee2beae79de5
gellmr <gellmr@users.noreply.github.com>
2025-08-05 01:26:27 +0800

    Remove unused method

--- ReactWithASP.Server/Controllers/MyAuthenticatedController.cs
+++ ReactWithASP.Server/Controllers/MyAuthenticatedController.cs
@@ -11,17 +11,5 @@ namespace ReactWithASP.Server.Controllers
     public MyAuthenticatedController(UserManager<AppUser> userManager){
       _userManager = userManager;
     }
-
-    protected async Task<AppUser> EnsureAppUser()
-    {
-      if (User.Identity.IsAuthenticated)
-      {
-        // Get the user's unique ID from the NameIdentifier claim
-        string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        AppUser appUser = await _userManager.FindByIdAsync(userId);
-        return appUser;
-      }
-      throw new Exception("Could not get AppUser");
-    }
   }
 }
