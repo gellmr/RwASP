82dfb4ca70a23d74a08a0d67c445d1da67dd2a06
gellmr <gellmr@users.noreply.github.com>
2025-08-06 17:53:58 +0800

    Markup for AdminOrder page

--- reactwithasp.client/src/Admin/AdminOrder.jsx
+++ reactwithasp.client/src/Admin/AdminOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useParams } from 'react-router';
+import { useParams, NavLink } from 'react-router';
 import { useSelector } from 'react-redux'
 import Spinner from 'react-bootstrap/Spinner';
 import AdminTitleBar from "@/Admin/AdminTitleBar";
@@ -16,29 +16,42 @@ function AdminOrder ()
   const adminOrders = useSelector(state => state.adminOrders.lines);
   const adminOrder = adminOrders.find(ord => ord.id == orderid);
 
-  const pageMarkup = (
+  const backLink = () => (
     <Row>
-      <Col xs={0}  sm={1}  md={2} lg={3}></Col>
-      <Col xs={12} sm={10} md={8} lg={6}>
-
-        <div className="AdminOrderDetailRow">
-          <Row className="">
-            <Col xs={6}>Order Number:</Col>  <Col xs={6}>{adminOrder.id}</Col>
-          </Row>
-          <Row className="">
-            <Col xs={6}>User Name:</Col>     <Col xs={6}>{adminOrder.username}</Col>
-          </Row>
-          <Row className="">
-            <Col xs={6}>User ID:</Col>       <Col xs={6}>{adminOrder.userID}</Col>
-          </Row>
-          <Row className="">
-            <Col xs={6}>Account Type:</Col>  <Col xs={6}>{adminOrder.accountType}</Col>
-          </Row>
-        </div>
-
+      <Col style={{ textAlign: 'left', marginBottom: 10 }}>
+        <NavLink to={"/admin/orders"} className="btn btn-light" style={{ textWrapMode: "nowrap", textDecoration: 'none', fontSize: 12 }}>
+          <i className="bi bi-arrow-left-short"></i> Back
+        </NavLink>
       </Col>
-      <Col xs={0} sm={1} md={2} lg={3}></Col>
     </Row>
+  );
+
+  const pageMarkup = (
+    <>
+      <Row>
+        <Col xs={0}  sm={1}  md={2} lg={3}></Col>
+        <Col xs={12} sm={10} md={8} lg={6}>
+
+          {backLink()}
+          <div className="AdminOrderDetailRow">
+            <Row className="">
+              <Col xs={6}>Order Number:</Col>  <Col xs={6}>{adminOrder.id}</Col>
+            </Row>
+            <Row className="">
+              <Col xs={6}>User Name:</Col>     <Col xs={6}>{adminOrder.username}</Col>
+            </Row>
+            <Row className="">
+              <Col xs={6}>User ID:</Col>       <Col xs={6}>{adminOrder.userID}</Col>
+            </Row>
+            <Row className="">
+              <Col xs={6}>Account Type:</Col>  <Col xs={6}>{adminOrder.accountType}</Col>
+            </Row>
+          </div>
+
+        </Col>
+        <Col xs={0} sm={1} md={2} lg={3}></Col>
+      </Row>
+    </>
   );
 
   const errMarkup = (
@@ -65,7 +78,7 @@ function AdminOrder ()
 
   return (
     <>
-      <AdminTitleBar titleText="Order Details" construction={true} />
+      <AdminTitleBar titleText={"Order #" + adminOrder.id} construction={true} />
       {isLoading ? loadingMarkup() : (error ? errMarkup : pageMarkup)}
     </>
   );
