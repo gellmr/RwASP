563bd143cfdf8c7aaabff34c96d824dac93f14b7
gellmr <gellmr@users.noreply.github.com>
2025-08-18 23:36:42 +0800

    Tests for homepage

--- NUnitTests/SeleniumTests/HomepageTests.cs
+++ NUnitTests/SeleniumTests/HomepageTests.cs
@@ -2,6 +2,8 @@
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
 using SeleniumExtras.WaitHelpers;
+using static System.Net.Mime.MediaTypeNames;
+using System;
 
 namespace NUnitTests.SeleniumTests
 {
@@ -31,21 +33,25 @@ namespace NUnitTests.SeleniumTests
     [Test]
     public void HomePage_LoadsSuccessfully()
     {
-      // Arrange & Act
-      // Navigate to the application's localhost URL.
       driver.Navigate().GoToUrl(viteUrl);
       try{
-        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(12));
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
         wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("a.storeBrand"))); // Wait for the heading to be present and visible on the page.
-      }catch (WebDriverTimeoutException){
-        Assert.Fail("The application page did not load correctly or the heading was not found.");
+        wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".productDetails")));
       }
-      Assert.That(driver.Title, Does.Contain("Shop"), "Home page title is incorrect.");
+      catch (WebDriverTimeoutException){
+        Assert.Fail("The application page did not load correctly, or the required element(s) were not found.");
+      }
       IWebElement heading = driver.FindElement(By.CssSelector("a.storeBrand"));
-      Assert.That(heading.Text, Is.EqualTo("SPORTS STORE"), "Heading text is incorrect.");
+      IReadOnlyCollection<IWebElement> products = driver.FindElements(By.CssSelector(".productDetails"));
+
+      Assert.That(driver.Title,          Does.Contain("Shop"),             "Home page title is incorrect.");
+      Assert.That(heading.Text,          Is.EqualTo("SPORTS STORE"),       "Heading text is incorrect.");
+      Assert.That(products.First().Text, Does.Contain("Drink Bottle $20"), "First product is present.");
+      Assert.That(products.Count,        Is.EqualTo(4),                    "4 products are on the page.");
     }
 
-    // The [TearDown] attribute runs after each test method to clean up resources.
+  // The [TearDown] attribute runs after each test method to clean up resources.
     [TearDown]
     public void TearDown()
     {
