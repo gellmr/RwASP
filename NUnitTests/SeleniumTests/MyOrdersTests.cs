54838995ae127c44acf9fdcde3e7777cc6e198fa
gellmr <gellmr@users.noreply.github.com>
2025-08-19 00:09:42 +0800

    Added MyOrders page test

---
+++ NUnitTests/SeleniumTests/MyOrdersTests.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+
+namespace NUnitTests.SeleniumTests
+{
+  [TestFixture]
+  public class MyOrdersTests : PageTest
+  {
+    [Test]
+    public void EmptyMyOrdersPage_LoadsSuccessfully()
+    {
+      driver.Navigate().GoToUrl(viteUrl + "/myorders");
+      string titleElement = ".shopLayoutTransparent.bgOrderFullTransparent h2";
+      try
+      {
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+        wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(titleElement))); // Wait for element to be visible on page.
+      }
+      catch (WebDriverTimeoutException)
+      {
+        Assert.Fail(pageOrElementMissing);
+      }
+      IWebElement element = driver.FindElement(By.CssSelector(titleElement));
+      Assert.That(element.Text, Does.Contain("My Orders"), "My Orders page - title is incorrect.");
+    }
+  }
+}
