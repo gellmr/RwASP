400d94882c0ff4a24434e7835bfe2affde4e1134
gellmr <gellmr@users.noreply.github.com>
2025-08-19 22:02:48 +0800

    Added IncrementInCart_QtyAndPriceUpdates selenium test

--- NUnitTests/SeleniumTests/CartTests.cs
+++ NUnitTests/SeleniumTests/CartTests.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
 using SeleniumExtras.WaitHelpers;
+using NUnitTests.Helpers;
 
 namespace NUnitTests.SeleniumTests
 {
@@ -10,12 +11,16 @@ namespace NUnitTests.SeleniumTests
     public string plusCss = ".addToCartBtnGroup button i.bi-plus";
     public string cartButtonCss = "a.mgNavLinkCartBtn";
     public string cartHasOneItem = "Cart: 1 Items";
+    public string cartHasTwoItem = "Cart: 2 Items";
     IWebElement smallBtn = null;
     IWebElement medBtn = null;
 
     IWebElement itemOneTitle = null;
     public string cartPageItemOneTitleCss = ".inCartProd .inCartItemText h6";
     public const string cartPageItemOneSuccessText = "Drink Bottle $20";
+
+    public string inCartRows = ".inCartProd";
+    public decimal itemOneUnitPrice = 20M;
 
     [Test]
     public void EmptyCartPage_LoadsSuccessfully()
@@ -99,9 +104,40 @@ namespace NUnitTests.SeleniumTests
       Assert.That(itemOneTitle.Text, Does.Contain(cartPageItemOneSuccessText), "Cart Page - Item One - text incorrect.");
     }
 
-    //[Test]
-    //public void IncrementInCart_QtyAndPriceUpdates()
-    //{
-    //}
+    
+
+    [Test]
+    public void IncrementInCart_QtyAndPriceUpdates()
+    {
+      var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+      ClickAddThenGoToCart();
+      if (itemOneTitle == null){ Assert.Fail("Cart Page - Item One - not found"); return; }
+      IWebElement summaryRow = null;
+      IWebElement row1 = null;
+      try
+      {
+        IReadOnlyCollection<IWebElement> cartRows = driver.FindElements(By.CssSelector(inCartRows));
+        List<IWebElement> rows = cartRows.ToList();
+        row1 = rows[0];
+        summaryRow = rows[rows.Count - 1];
+        IWebElement addBtn = row1.FindElement(By.CssSelector(".inCartItemRemove i.bi-plus"));
+        IWebElement clickableButton = wait.Until(ExpectedConditions.ElementToBeClickable(addBtn));
+        clickableButton.Click(); // Add a second item
+        wait.Until(ExpectedConditions.TextToBePresentInElement(medBtn, cartHasTwoItem));
+      }
+      catch (WebDriverTimeoutException){
+        Assert.Fail("Failed within Cart during IncrementInCart_QtyAndPriceUpdates test.");
+      }
+      if (medBtn == null) {     Assert.Fail("Cart (medBtn)     not found"); return; }
+      if (row1 == null) {       Assert.Fail("Cart (row1)       not found"); return; }
+      if (summaryRow == null) { Assert.Fail("Cart (summaryRow) not found"); return; }
+      string? row1Text    = TestHelpers.TrimAndFlattenString(row1.Text);
+      string? summaryText = TestHelpers.TrimAndFlattenString(summaryRow.Text);
+      string priceTot = (2 * itemOneUnitPrice).ToString("G29");
+      Assert.That(medBtn.Text,     Does.Contain(cartHasTwoItem),                    "Cart (medBtn)     - Updated text is incorrect.");
+      Assert.That(row1Text,        Does.Contain("Quantity: 2 Price: $" + priceTot), "Cart (row1)       - Qty text is incorrect.");
+      Assert.That(summaryText,     Does.Contain("Total: 2 Items $ " + priceTot),    "Cart (summaryRow) - Qty text is incorrect.");
+      Assert.That(summaryRow.Text, Does.Contain("$ " + priceTot),                   "Cart (summaryRow) - Price text is incorrect.");
+    }
   }
 }
