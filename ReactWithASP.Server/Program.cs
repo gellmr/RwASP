a18ef1f56322daf878d152a5ae2678bbeb52e6e9
gellmr <gellmr@users.noreply.github.com>
2025-08-19 17:05:06 +0800

    Remove app.UseSwagger

--- ReactWithASP.Server/Program.cs
+++ ReactWithASP.Server/Program.cs
@@ -88,13 +88,6 @@ var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()){
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
