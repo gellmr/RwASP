3e1cd531643fc03c494021621461c41688664565
gellmr <gellmr@users.noreply.github.com>
2025-08-19 22:48:55 +0800

    Added EmptyCheckoutPage_LoadsSuccessfully

---
+++ NUnitTests/SeleniumTests/CheckoutTests.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace NUnitTests.SeleniumTests
+{
+  [TestFixture]
+  internal class CheckoutTests: PageTest
+  {
+    string titleElement = ".shopLayoutTransparent h2";
+
+    [Test]
+    public void EmptyCheckoutPage_LoadsSuccessfully()
+    {
+      driver.Navigate().GoToUrl(viteUrl + "/checkout");
+      var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+      try{
+        wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(titleElement)));
+      }
+      catch (WebDriverTimeoutException){
+        Assert.Fail(pageOrElementMissing);
+      }
+      IWebElement element = driver.FindElement(By.CssSelector(titleElement));
+      Assert.That(element.Text, Does.Contain("Checkout"), "Checkout - title is incorrect.");
+    }
+  }
+}
