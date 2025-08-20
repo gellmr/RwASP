ec3ed45cbdbeb1d2ba584c4a18b76a3b808d8285
gellmr <gellmr@users.noreply.github.com>
2025-08-20 13:34:24 +0800

    Method to find FindProjectRoot for loading app settings json

---
+++ NUnitTests/Helpers/MgUtility.cs
@@ -0,0 +1,24 @@
+﻿
+namespace NUnitTests.Helpers
+{
+  public static class MgUtility
+  {
+    public static string? FindProjectRoot()
+    {
+      string? currentDirectory = AppContext.BaseDirectory;
+      while (currentDirectory != null)
+      {
+        // Search for a .csproj file in the current directory
+        string[] files = Directory.GetFiles(currentDirectory, "*.csproj");
+        if (files.Length > 0)
+        {
+          return currentDirectory;
+        }
+
+        // Move up to the parent directory
+        currentDirectory = Directory.GetParent(currentDirectory)?.FullName;
+      }
+      return null; // Project root not found
+    }
+  }
+}
