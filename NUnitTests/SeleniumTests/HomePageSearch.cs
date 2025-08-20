204992a7c8c7cae53995e704c4e8d1a9351cfa07
gellmr <gellmr@users.noreply.github.com>
2025-08-20 16:25:25 +0800

    Update HomePageSearch.cs

--- NUnitTests/SeleniumTests/HomePageSearch.cs
+++ NUnitTests/SeleniumTests/HomePageSearch.cs
@@ -15,6 +15,8 @@ namespace NUnitTests.SeleniumTests
     [Test]
     public void TypeSearch_BringsProductResults()
     {
+      IWebElement? product1 = null;
+      IWebElement? product2 = null;
       driver.Navigate().GoToUrl(viteUrl);
       const string searchCss = "input[placeholder='Search for products']";
       try
@@ -24,15 +26,17 @@ namespace NUnitTests.SeleniumTests
         searchElement.SendKeys("fla");
         IReadOnlyCollection<IWebElement> products = driver.FindElements(By.CssSelector(".productDetails"));
         List<IWebElement> prods = products.ToList();
-        IWebElement product1 = prods[0];
-        IWebElement product2 = prods[1];
-        Assert.That(product1.Text, Does.Contain("Soccer Stadium $80000"), "First product - is missing.");
-        Assert.That(product2.Text, Does.Contain("Corner Flags $25"), "Second product - is missing.");
+        product1 = prods[0];
+        product2 = prods[1];
       }
       catch (WebDriverTimeoutException)
       {
         Assert.Fail("Timed out waiting for elements or page navigation.");
       }
+      if (product1 == null){ Assert.Fail("BringsProductResults - product1 did not appear."); return; }
+      if (product2 == null) { Assert.Fail("BringsProductResults - product2 did not appear."); return; }
+      Assert.That(product1.Text, Does.Contain("Soccer Stadium $80000"), "BringsProductResults - First product - is missing.");
+      Assert.That(product2.Text, Does.Contain("Corner Flags $25"), "BringsProductResults - Second product - is missing.");
     }
   }
 }
