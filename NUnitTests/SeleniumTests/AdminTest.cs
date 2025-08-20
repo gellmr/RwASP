50b9a66afc498542b5979894db929b9b6fc95871
gellmr <gellmr@users.noreply.github.com>
2025-08-20 13:44:54 +0800

    Update AdminTest.cs

--- NUnitTests/SeleniumTests/AdminTest.cs
+++ NUnitTests/SeleniumTests/AdminTest.cs
@@ -8,7 +8,17 @@ namespace NUnitTests.SeleniumTests
 
     public void GoToLoginPage()
     {
+      driver.Navigate().GoToUrl(viteUrl + "/admin");
 
+      // Wait for login elements to be visible
+
+      // Populate login fields
+
+      // Click login button
+
+      // Wait for backlog page to appear
+
+      // Login page should have appeared.
     }
 
     public void LoginAsVip()
