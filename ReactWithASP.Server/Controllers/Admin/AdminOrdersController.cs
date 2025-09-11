1a0d458a5addf9cf1319bbd6e629be4e24ae5199
gellmr <gellmr@users.noreply.github.com>
2025-09-11 18:03:47 +0800

    Tidy code

--- ReactWithASP.Server/Controllers/Admin/AdminOrdersController.cs
+++ ReactWithASP.Server/Controllers/Admin/AdminOrdersController.cs
@@ -21,40 +21,21 @@ namespace ReactWithASP.Server.Controllers.Admin
     {
       try
       {
-        // Validate controller arguments
-
-        // If arguments missing, use ones from session.
-
-        // If this is the first time page has been requested and session + arguments are missing, initialise arguments to default.
-
         IEnumerable<AdminOrderRow> rows = await orderRepo.GetOrdersWithUsersAsync(pageNum);
         if (rows == null || !rows.Any()){
           BadRequest(new { errMessage = "Something went wrong. Records not found." });
         }
-
-        /*
-        // Get list of orders
-        IEnumerable<Order> currentPageOrders = orderRepo.GetOrdersWithUsersAsync()
-          //.OrderBy(order => order.UserID);
-          .OrderBy(order => order.ID);
-
-        IEnumerable<Order> guestOrders       = currentPageOrders.Where(ord => (ord.UserID == null) && (ord.GuestID != null)).OrderBy(order => order.UserID);
-        IEnumerable<Order> appUserOrders     = currentPageOrders.Where(ord => (ord.UserID != null) && (ord.GuestID == null)).OrderBy(order => order.UserID);
-        */
-
-        // If not specified, display Guest orders before AppUser orders.
-        //List<Order> sorted = [.. guestOrders, .. appUserOrders];
-
-        // Persist arguments to the session
-
-        // Apply sorting here, according to what the user wants.
-        List<OrderSlugDTO> sorted = rows
-          .OrderBy(o => o.OrderPlaced).Reverse()
-          .Select(order => order.OrderSlug)
-          .ToList();
-        bool success = true;
-        if (success){
-          return Ok(new { orders = sorted }); // Automatically cast object to JSON.
+        else
+        {
+          // Apply sorting here, according to what the user wants.
+          List<OrderSlugDTO> sorted = rows
+            .OrderBy(o => o.OrderPlaced).Reverse()
+            .Select(order => order.OrderSlug)
+            .ToList();
+          bool success = true;
+          if (success){
+            return Ok(new { orders = sorted }); // Automatically cast object to JSON.
+          }
         }
       }
       catch(Exception){
